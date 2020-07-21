a6da6914d8709ed94e4bc169a3701fe97df2a66d
Sujin Lee <krispediadot@gmail.com>
2020-07-21 15:21:39 +0900

    Mod: Flyweight_Pattern.cpp

--- Flyweight_Pattern/Flyweight_Pattern.cpp
+++ Flyweight_Pattern/Flyweight_Pattern.cpp
@@ -1,50 +1,51 @@
 #include<iostream>
-#include<vector>
+#include<map>
 
 using namespace std;
 
 class Flyweight{
 public:
-
     virtual ~Flyweight(){}
-    virtual void Operation()=0;
+    virtual void Operation(string extrinsicState)=0;
 };
 class ConcreteFlyweight: public Flyweight{
 public:
-    void Operation() override {cout<<"Concrete Flyweight"<<endl;}
+    void Operation(string extrinsicState) override {cout<<"Concrete Flyweight "<<extrinsicState<<endl;}
 };
 class UnsharedConcreteFlyweight: public Flyweight{
 public:
-    void Operation() override {cout<<"UnsharedConcreteFlyweight"<<endl;}
+    void Operation(string extrinsicState) override {cout<<"UnsharedConcreteFlyweight "<<extrinsicState<<endl;}
 };
 
 class FlyweightFactory{
 private:
-    vector<pair<int,Flyweight*>> _pool;
+    map<int, Flyweight*> pool;
 public:
     Flyweight* getFlyweight(int key){
-        for(auto flyweight: _pool){
-            if(flyweight.first == key) {
-                cout<<"Found flyweight "<<flyweight.second<<endl;
-                return flyweight.second;
-            }
+        if(pool.find(key) != pool.end()) {
+            cout<<"Found flyweight "<<pool[key]<<endl;
+            return pool[key];
         }
+
         Flyweight* newFlyweight = new ConcreteFlyweight();
-        _pool.push_back(make_pair(key, newFlyweight));
+        pool[key] = newFlyweight;
         cout<<"Created new flyweight "<<newFlyweight<<endl;
-        return newFlyweight;
+        return pool[key];
     }
 };
 
 int main(){
-    FlyweightFactory fFactory;
-    Flyweight* flyweight = fFactory.getFlyweight(1);
-    flyweight = fFactory.getFlyweight(1);
-    flyweight->Operation();
+    FlyweightFactory* fFactory = new FlyweightFactory();
+    Flyweight* flyweight = fFactory->getFlyweight(1);
+    flyweight = fFactory->getFlyweight(1);
+    flyweight->Operation("extrinsic1");
+    flyweight->Operation("extrinsic2");
+    cout<<endl;
+    flyweight = fFactory->getFlyweight(2);
+    flyweight = fFactory->getFlyweight(2);
+    flyweight->Operation("extrinsic2");
 
-    flyweight = fFactory.getFlyweight(2);
-    flyweight = fFactory.getFlyweight(2);
-
+    delete fFactory;
     delete flyweight;
 
     return 0;
